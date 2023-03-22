c760694501ecc6382201ee060e03d7050a1eeb12
RickFerreira <richard.ferreira.ext@synchro.com.br>
2023-03-22 16:45:33 -0300

    Criando o projeto base

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Olá Mundo!!</h1>
+      Esse aqui vai ser o meu novo projeto, em que vou fazer uma aplicação para ajudar alunos do curso de Engenharia da Computação do IFPB    
+    </div>
+  )
+}
+
+export default App
