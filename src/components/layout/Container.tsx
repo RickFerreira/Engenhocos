31c856406872e4609491417c082c1d3558027c99
RickFerreira <richard.ferreira.ext@synchro.com.br>
2023-04-25 14:21:55 -0300

    Resolvendo a tipagem da prop

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -1,6 +1,6 @@
 import styles from './Container.module.css'
 
-function Container(props) {
+function Container(props: any) {
   return (
     <div className={`${styles.container} ${styles[props.customClass]}`}>
       {props.children}
